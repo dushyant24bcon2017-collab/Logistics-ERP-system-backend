858b08ea6b416a943fb7d2105b151ea1d43861da
Dushyant Singh <dushyant.24bcon2017@jecrcu.edu.in>
2026-06-12 21:47:48 +0530

    changes in tsconfog for deployment again,last few changes

--- src/generated/models.ts
+++ src/generated/models.ts
@@ -8,10 +8,10 @@
  *
  * 🟢 You can import this file directly.
  */
-export type * from './models/Tenant'
-export type * from './models/User'
-export type * from './models/Product'
-export type * from './models/Customer'
-export type * from './models/Order'
-export type * from './models/OrderItem'
-export type * from './commonInputTypes'
+export type * from './models/Tenant.js'
+export type * from './models/User.js'
+export type * from './models/Product.js'
+export type * from './models/Customer.js'
+export type * from './models/Order.js'
+export type * from './models/OrderItem.js'
+export type * from './commonInputTypes.js'
